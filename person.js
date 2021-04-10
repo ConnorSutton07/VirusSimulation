9ef3fb8fca2f744c6537dca7cf84f093aee58589
Connor Sutton <connorsutton07@gmail.com>
2021-04-10 10:07:08 -0500

    added more sliders and connected to corresponding variables

--- person.js
+++ person.js
@@ -5,7 +5,7 @@ class Person
         self.vaccinated = false;
         self.home_location = home;
         self.current_location = home;
-        self.mask_wearer = (Math.random() < 0.75)
+        self.mask_wearer = (Math.random() < document.getElementById('rateMasks').value)
         self.infected = Math.random() < 0.10;
         self.action = 'idle';
     }
