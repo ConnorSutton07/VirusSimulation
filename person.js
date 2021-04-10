0c4bf3a52a43358c2584bb1613caaf2d9bb0876c
Connor Sutton <connorsutton07@gmail.com>
2021-04-10 18:01:52 -0500

    added ability to migrate

--- person.js
+++ person.js
@@ -6,11 +6,9 @@ class Person
         this.home_location = home;
         this.current_location = home;
         this.destination = home;
-        this.mask_wearer = Math.random() > (document.getElementById('rateMasks').value / 100)
-        this.social_distancer = Math.random() > (document.getElementById('socialDistancing').value / 100)
-        this.migration_change = document.getElementById('rateMigration').value / 100;
-        this.death_chance = document.getElementById('lethality').value / 100000;
-        this.infected = Math.random() < 0.05;
+        this.mask_wearer = Math.random() > MASK_RATE;
+        this.social_distancer = SOCIAL_DISTANCE_RATE;
+        this.infected = 0; //Math.random() < 0.05;
         this.been_infected = this.infected
         this.time_infected = 0
         this.action = 'idle';
@@ -35,11 +33,27 @@ class Person
         return true;
     }
 
-    getDestination(buildings) {
+    getDestination(people, buildings) {
         this.action = 'idle';
-        if (Math.random() < this.migration_change)
+
+        if (this.isMigrating)
+        {
+            let new_y = 0;
+            do new_y = Math.floor(Math.random() * 100); while (people[0][new_y] != 0)
+            people[0][new_y] = this;
+            people[this.current_location[0]][this.current_location[1]] = 0;
+            this.current_location = [0, new_y];
+            this.isMigrating = false;
+            this.infected = Math.random() < 0.5;
+            this.action = 'moving';
+        }
+
+
+        if (Math.random() < MIGRATION_RATE)
         {
             this.isMigrating = true;
+            console.log("Migrator");
+            return [GRID_SIZE - 1, Math.floor(Math.random() * 100)];
         }
         if (!(this.current_location[0] == this.home_location[0] && this.current_location[1] == this.home_location[1])) { // if not at home
             if (Math.random() > 0.25) {
@@ -58,7 +72,7 @@ class Person
         this.updated = true;
         let pos = [this.current_location[0], this.current_location[1]]
         if (pos[0] == this.destination[0] && pos[1] == this.destination[1]) {
-            this.destination = this.getDestination(buildings)
+            this.destination = this.getDestination(people, buildings)
             return;
         }
         if (!this.dead) {
@@ -79,7 +93,7 @@ class Person
                 {
                     people[new_x][new_y] = this;
                     people[pos[0]][pos[1]] = 0;
-                    this.current_location = [new_x, new_y]    
+                    this.current_location = [new_x, new_y];
                 }
                 else
                 {
@@ -87,23 +101,23 @@ class Person
                 }    
 
             }
-            if (this.time_infected == 200) {
-                this.been_infected = true
-                this.infected = false
+            if (this.time_infected == INFECTION_DURATION) {
+                this.been_infected = true;
+                this.infected = false;
             }
 
             if (this.infected) {
-                this.infect(people, houses, buildings) 
-                this.death()
-                this.time_infected += 1
+                this.infect(people, houses, buildings) ;
+                this.death();
+                this.time_infected += 1;
             }
             else {
-                this.time_infected = 0
+                this.time_infected = 0;
             }
         }
     }
     death() {
-        if (Math.random() < this.death_chance) {
+        if (Math.random() < DEATH_RATE) {
             this.dead = true;
         }
     }
